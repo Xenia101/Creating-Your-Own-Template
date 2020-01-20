0497bbc1710485e553672ec5d780c1366a99c6fd
Xenia <lommy101@gmail.com>
2020-01-20 15:10:07 +0900

    new commit

--- sample.py
+++ sample.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
     return render_template('index.html')
+
+@app.route('/result',methods = ['POST', 'GET'])
+def getuser():
+   if request.method == 'POST':
+      result = request.form
+      print(result['Title'])
+      return render_template("index.html",result = result)
 
 if __name__ == '__main__':
     app.run(port=5000)
