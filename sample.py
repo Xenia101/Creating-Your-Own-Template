6cea57c7cf4c17ed38484d1705eec794260ec259
Xenia <lommy101@gmail.com>
2020-01-30 04:05:02 +0900

    fixed something

--- sample.py
+++ sample.py
@@ -11,25 +11,38 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 def home():
     return render_template('index.html')
 
+color = '#5bc0de'
+imgpath = ''
+
 @app.route('/result',methods = ['POST', 'GET'])
 def getuser():
    if request.method == 'POST':
-      result = request.form
-      
-      print(result)
-      return render_template("index.html",result = result)
+       global color
+       global imgpath
+       result = request.form
+       print(result)
+       print(color)
+       print(imgpath)
+
+       return render_template("index.html",result = result)
 
 @app.route('/color', methods=['POST'])
 def color():
-     value = request.form['color']
-     print(value)
-     return '', 204
+    if request.method == 'POST':
+        global color
+        value = request.form['color']
+        color = str(value)
+        return '', 204
      
 @app.route('/option', methods=['POST'])
 def option():
     if request.method == 'POST':
+        global imgpath
+        global color
+        color = '#5bc0de'
         isthisFile=request.files.get('file')
         isthisFile.save("./file/"+isthisFile.filename)
+        imgpath = './file/' + isthisFile.filename
         return jsonify({'file': 'true'})
     
 if __name__ == '__main__':
