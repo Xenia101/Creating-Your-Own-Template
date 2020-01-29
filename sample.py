5984e6edbe9ca1b5115e0a59aab762012dd10f0e
Xenia <lommy101@gmail.com>
2020-01-30 03:34:12 +0900

    add ajax and flask update for image upload

--- sample.py
+++ sample.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, jsonify
+import json
 
 app = Flask(__name__)
+UPLOAD_FOLDER = 'C:\\Users\\User\\Desktop\\Creating-Your-Own-Template\\file'
+app.secret_key = "XENIA"
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def home():
@@ -10,8 +15,16 @@ def home():
 def getuser():
    if request.method == 'POST':
       result = request.form
+      
       print(result)
       return render_template("index.html",result = result)
 
+@app.route('/option', methods=['POST'])
+def option():
+    if request.method == 'POST':
+        isthisFile=request.files.get('file')
+        isthisFile.save("./file/"+isthisFile.filename)
+        return jsonify({'file': 'true'})
+    
 if __name__ == '__main__':
     app.run(port=5000)
